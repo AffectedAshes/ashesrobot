83deca109c24f9ab9ef7a4bb7f56b8292b9d1b8f
unknown <florian.hinck190@gmail.com>
2023-11-22 12:57:41 +0100

    update

--- commands/handlers/commandList.js
+++ commands/handlers/commandList.js
@@ -105,7 +105,7 @@ const commandList = {
   },
   '!chatgpt': {
     cooldown: true,
-    cooldownDuration: 600,
+    cooldownDuration: 300,
     execute: processChatGPTCommand,
   },
   '!metronome': {
