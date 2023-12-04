93272017fc2bd36928864eceb92c50535554c0be
unknown <florian.hinck190@gmail.com>
2023-12-04 21:41:50 +0100

    updated define response

--- commands/complex-cmds/define.js
+++ commands/complex-cmds/define.js
@@ -16,8 +16,15 @@ async function defineCommand(target, client, context, msg) {
     const data = response.data;
 
     if (data.list && data.list.length > 0) {
-      const definition = data.list[0].definition;
-      client.say(target, `@${context.username} Definition of "${word}": ${definition}`);
+      const shortEnoughDefinitions = data.list.filter(definitionData => definitionData.definition.length <= 500);
+
+      if (shortEnoughDefinitions.length > 0) {
+        const randomIndex = Math.floor(Math.random() * shortEnoughDefinitions.length);
+        const chosenDefinition = shortEnoughDefinitions[randomIndex].definition;
+        client.say(target, `@${context.username} Definition of "${word}": ${chosenDefinition}`);
+      } else {
+        client.say(target, `@${context.username} No short enough definition found for "${word}".`);
+      }
     } else {
       client.say(target, `@${context.username} No definition found for "${word}".`);
     }
