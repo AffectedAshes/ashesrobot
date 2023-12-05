e9aa293dc9128af09f36396f00f21abff72d3e2d
unknown <florian.hinck190@gmail.com>
2023-12-05 13:20:58 +0100

    improved define randomness/error logging

--- commands/complex-cmds/define.js
+++ commands/complex-cmds/define.js
@@ -1,8 +1,11 @@
 // define.js
 
 const axios = require('axios');
-
+const { Random } = require('random-js');
 const { sanitizeInput } = require('../handlers/sanitizer');
+
+// Create a new Random instance
+const random = new Random();
 
 // Define command with input sanitization
 async function defineCommand(target, client, context, msg) {
@@ -16,11 +19,10 @@ async function defineCommand(target, client, context, msg) {
     const data = response.data;
 
     if (data.list && data.list.length > 0) {
-      const shortEnoughDefinitions = data.list.filter(definitionData => definitionData.definition.length <= 500);
+      const shortEnoughDefinitions = data.list.filter(definitionData => definitionData.definition.length <= 750);
 
       if (shortEnoughDefinitions.length > 0) {
-        const randomIndex = Math.floor(Math.random() * shortEnoughDefinitions.length);
-        const chosenDefinition = shortEnoughDefinitions[randomIndex].definition;
+        const chosenDefinition = rndDefinition(shortEnoughDefinitions);
         client.say(target, `@${context.username} Definition of "${word}": ${chosenDefinition}`);
       } else {
         client.say(target, `@${context.username} No short enough definition found for "${word}".`);
@@ -29,9 +31,19 @@ async function defineCommand(target, client, context, msg) {
       client.say(target, `@${context.username} No definition found for "${word}".`);
     }
   } catch (error) {
-    console.error('Error fetching definition:', error);
+    if (axios.isAxiosError(error)) {
+      console.error(`Axios HTTP error: ${error.message}`);
+    } else {
+      console.error('Error fetching definition:', error.message);
+    }
     client.say(target, `@${context.username} An error occurred while fetching the definition.`);
   }
+}
+
+// Function to generate a random definition
+function rndDefinition(definitions) {
+  const rndNum = random.integer(0, definitions.length - 1);
+  return definitions[rndNum].definition;
 }
 
 module.exports = {
